85dbbce2024ac210016b300632c747671e366e42
lykosic <105546116+lykosic@users.noreply.github.com>
2022-05-14 19:59:35 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,186 @@
 #include <iostream>
 
 using namespace std;
-
-int main ()
+class User
 {
-    cout << "Hello World \n";
-    
-    
-    
-    return o;
+private:
+	string username;
+	string password;
+public:
+	//constructors
+	User()
+	{
+		username[0] = '\0';
+		password[0] = '\0';
+	}
+	User(string newName, string newPassword)
+	{
+		username = newName;
+		password = newPassword;
+	}
+	User(const User& newUser)
+	{
+		username = newUser.username;
+		password = newUser.password;
+
+	}
+	//setters
+	void setUsername(string newName)
+	{
+		username = newName;
+	}
+	void setPassword(string newPassword)
+	{
+		password = newPassword;
+	}
+	//getters
+	string getUsername()
+	{
+		return username;
+	}
+	string getPassword()
+	{
+		return password;
+
+	}
+	// Print Function
+};
+
+class Passenger :public User
+{
+private:
+	string passengerName;
+	string nationality;
+	long long int passportNo;
+	int ticketsBooked;
+	int* ticketId;
+public:
+
+	//Constructors
+	Passenger()
+	{
+		passengerName[0] = '\0';
+		nationality[0] = '\0';
+		passportNo = 0;
+		ticketsBooked = 0;
+		ticketId = 0;
+	}
+	Passenger(string newName, string newPassword, string newPassengerName, string newNationality, 
+		long long int newPassportNo, int totaltickets, int* newticketid): User(newName, newPassword)
+	{
+		passengerName = newPassengerName;
+		nationality = newNationality;
+		passportNo = newPassportNo;
+		ticketsBooked = totaltickets;
+		ticketId = new int[ticketsBooked];
+		for (int i = 0; i < ticketsBooked; i++)
+		{
+			ticketId[i] = newticketid[i];
+		}
+	}
+	Passenger(const User &newUser, const Passenger& newPassenger):User(newUser)
+	{
+		passengerName = newPassenger.passengerName;
+		nationality = newPassenger.nationality;
+		passportNo = newPassenger.passportNo;
+		ticketsBooked = newPassenger.ticketsBooked;
+		ticketId = new int[ticketsBooked];
+		for (int i = 0; i < ticketsBooked; i++)
+		{
+			ticketId[i] = newPassenger.ticketId[i];
+		}
+	}
+
+
+	//Setters
+	void SetPassengerName(string newPassengerName)
+	{
+		passengerName = newPassengerName;
+	}
+	void SetNationality(string newNationality)
+	{
+		nationality = newNationality;
+	}
+	void SetPassportNo(long long int newPassportNo)
+	{
+		passportNo = newPassportNo;
+	}
+	void SetTicketsBooked(int totaltickets)
+	{
+		ticketsBooked = totaltickets;
+	}
+	void SetTicketIds(int* newticketid)
+	{
+		ticketId = new int[ticketsBooked];
+		for (int i = 0; i < ticketsBooked; i++)
+		{
+			ticketId[i] = newticketid[i];
+		}
+	}
+
+	//Getters
+	string GetPassengerName() 
+	{ 
+		return passengerName;
+	}
+	string GetNationality()
+	{ 
+		return nationality; 
+	}
+	long long int GetPassportNo() 
+	{
+		return passportNo; 
+	}
+	int GetTicketsBooked() 
+	{
+		return ticketsBooked; 
+	}
+	int* GetTicketIds() 
+	{
+		return ticketId; 
+	}
+
+
+
+};
+class Admin : public User
+{
+private:
+	string adminName;
+public:
+
+	//constructors
+	Admin()
+	{
+		adminName[0] ='\0';
+
+	}
+	Admin(string newName, string newPassword, string newAdminName) :User(newName, newPassword)
+	{
+		adminName = newAdminName;
+	}
+	Admin(const User &newUser, const Admin& newAdmin):User(newUser)
+	{
+		adminName = newAdmin.adminName;
+	}
+	//setter
+	void SetAdminName(string newAdminName)
+	{
+		adminName = newAdminName;
+	}
+
+	//getter
+	string GetAdminName()
+	{
+		return adminName;
+	}
+
+
+
+};
+
+
+int main()
+{
+	return 0;
 }
