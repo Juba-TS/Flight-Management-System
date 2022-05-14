76e8e44dc1d3e86140a7a93d262597f54db11a14
FlyHercules <62466015+FlyHercules@users.noreply.github.com>
2022-05-14 20:45:35 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-class Ticket 
+class Ticket
 {
 private:
 	int ticketID;
@@ -35,29 +35,99 @@ public:
 		return passportNumber;
 	}
 };
-class User
+class Flight
 {
 private:
-	string username;
-	string password;
+	Ticket* ticketArray;
+	string airline;
+	string country;
+	char statusOfFlight;	//O for ontime, D for delayed, M for Missed
+	char type;	// D for domestic I for international
+	int availableTickets;
+	int flightNumber;
+	int departureTime[3];	// hr min sec
+	int arrivalTime[3];
+	int date[3];		//day month year
 public:
-	User()
+	Flight()
 	{
-		username[0] = '\0';
-		password[0] = '\0';
+		ticketArray = NULL;
+		airline = '\0';
+		country = '\0';
+		statusOfFlight = '\0';
+		type = '\0';
+		availableTickets = 0;
+		flightNumber = 0;
+		for (int counter = 0;counter < 3;counter++)
+		{
+			departureTime[counter] = 0;
+			arrivalTime[counter] = 0;
+		}
 	}
-	User(string newName, string newPassword)
+	Flight(Ticket* newArr, string newAirline, string newCountry,
+		char newStatus, char newType, int newTickets, int newFlightNumber, int newDeptTime[], int newArrTime[],int newDates[])
 	{
-		username = newName;
-		password = newPassword;
+		ticketArray = newArr;
+		airline = newAirline;
+		country = newCountry;
+		statusOfFlight = newStatus;
+		type = newType;
+		availableTickets = newTickets;
+		flightNumber = newFlightNumber;
+		for (int counter = 0;counter < 3;counter++)
+		{
+			departureTime[counter] = newDeptTime[counter];
+			arrivalTime[counter] = newArrTime[counter];
+		}
 	}
-	void setUsername(string newName);
-	void setPassword(string newPassword);
-	string getUsername();
-	string getPassword();
-	// Print Function
-};
 
+	// Setters
+	void setTicketArr(Ticket* newArr)
+	{
+		ticketArray = newArr;
+	}
+	void setAirline(string newAirline)
+	{
+		airline = newAirline;
+	}
+	void setCountry(string newCountry)
+	{
+		country = newCountry;
+	}
+	void setStatusOfFlight(char newStatus)
+	{
+		statusOfFlight = newStatus;
+	}
+	void setType(char newType)
+	{
+		type = newType;
+	}
+	void setAvailableTickets(int newAvailableTickets)
+	{
+		availableTickets = newAvailableTickets;
+	}
+	void setFlightNumber(int newFlight)
+	{
+		flightNumber = newFlight;
+	}
+	void setDepartureTime(int newDeptTimes[])
+	{
+		for (int counter = 0;counter < 3;counter++)
+			departureTime[counter] = newDeptTimes[counter];
+	}
+	void setArrivalTime(int newArrivalTimes[])
+	{
+		for (int counter = 0;counter < 3;counter++)
+			arrivalTime[counter] = newArrivalTimes[counter];
+	}
+	void setDate(int newDates[])
+	{
+		for (int counter = 0;counter < 3;counter++)
+			date[counter] = newDates[counter];
+	}
+
+	// Getters
+}
 int main ()
 {
     cout << "Hello World \n";
