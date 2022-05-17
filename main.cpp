7b759369fbae9db493bbce9aa05ca766d897a940
lykosic <105546116+lykosic@users.noreply.github.com>
2022-05-18 02:31:42 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-
 using namespace std;
+
+
+
 class User
 {
 private:
@@ -41,11 +43,28 @@ public:
 	string getPassword()
 	{
 		return password;
-
+	}
+	
+	bool operator ==(const User& newUser)
+	{
+		if (username == newUser.username && password == newUser.password)
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	bool operator !=(const User& newUser)
+	{
+		if (username != newUser.username && password != newUser.password)
+		{
+			return true;
+		}
+		else
+			return false;
 	}
 	// Print Function
 };
-
 class Passenger :public User
 {
 private:
@@ -65,8 +84,8 @@ public:
 		ticketsBooked = 0;
 		ticketId = 0;
 	}
-	Passenger(string newName, string newPassword, string newPassengerName, string newNationality, 
-		long long int newPassportNo, int totaltickets, int* newticketid): User(newName, newPassword)
+	Passenger(string newName, string newPassword, string newPassengerName, string newNationality,
+		long long int newPassportNo, int totaltickets, int* newticketid) : User(newName, newPassword)
 	{
 		passengerName = newPassengerName;
 		nationality = newNationality;
@@ -78,7 +97,7 @@ public:
 			ticketId[i] = newticketid[i];
 		}
 	}
-	Passenger(const User &newUser, const Passenger& newPassenger):User(newUser)
+	Passenger(const User& newUser, const Passenger& newPassenger) :User(newUser)
 	{
 		passengerName = newPassenger.passengerName;
 		nationality = newPassenger.nationality;
@@ -119,30 +138,28 @@ public:
 	}
 
 	//Getters
-	string GetPassengerName() 
-	{ 
+	string GetPassengerName()
+	{
 		return passengerName;
 	}
 	string GetNationality()
-	{ 
-		return nationality; 
-	}
-	long long int GetPassportNo() 
 	{
-		return passportNo; 
+		return nationality;
 	}
-	int GetTicketsBooked() 
+	long long int GetPassportNo()
 	{
-		return ticketsBooked; 
+		return passportNo;
 	}
-	int* GetTicketIds() 
+	int GetTicketsBooked()
 	{
-		return ticketId; 
+		return ticketsBooked;
 	}
-
-
-
+	int* GetTicketIds()
+	{
+		return ticketId;
+	}
 };
+
 class Admin : public User
 {
 private:
@@ -152,14 +169,14 @@ public:
 	//constructors
 	Admin()
 	{
-		adminName[0] ='\0';
+		adminName[0] = '\0';
 
 	}
 	Admin(string newName, string newPassword, string newAdminName) :User(newName, newPassword)
 	{
 		adminName = newAdminName;
 	}
-	Admin(const User &newUser, const Admin& newAdmin):User(newUser)
+	Admin(const User& newUser, const Admin& newAdmin) :User(newUser)
 	{
 		adminName = newAdmin.adminName;
 	}
@@ -175,12 +192,431 @@ public:
 		return adminName;
 	}
 
-
-
+	
 };
+
+class Ticket
+{
+private:
+	int ticketID;
+	int passportNumber;
+public:
+	Ticket()
+	{
+		ticketID = 0;
+		passportNumber = 0;
+	}
+	Ticket(int newID, int newPass)
+	{
+		ticketID = newID;
+		passportNumber = newPass;
+	}
+	void setID(int newID)
+	{
+		ticketID = newID;
+	}
+	void setPassportNumber(int newPass)
+	{
+		passportNumber = newPass;
+	}
+	int getID()
+	{
+		return ticketID;
+	}
+	int getPassportNumber()
+	{
+		return passportNumber;
+	}
+};
+
+class Date
+{
+public:
+	int day;
+	int month;
+	int year;
+
+	Date()
+	{
+		day = 0;
+		month = 0;
+		year = 0;
+	}
+	Date(int newDay, int newMonth, int newYear)
+	{
+		day = newDay;
+		month = newMonth;
+		year = newYear;
+	}
+};
+
+class Time
+{
+public:
+	int hour;
+	int minutes;
+	int seconds;
+
+	Time()
+	{
+		hour = 0;
+		minutes = 0;
+		seconds = 0;
+	}
+	Time(int newHours, int newMinutes, int newSeconds)
+	{
+		hour = newHours;
+		minutes = newMinutes;
+		seconds = newSeconds;
+	}
+};
+
+class Airline
+{
+private:
+	string airlineName;
+	int airlineID;
+public:
+	Airline()
+	{
+		airlineName = '\0';
+		airlineID = 0;
+	}
+	Airline(string newName, int newID)
+	{
+		airlineName = newName;
+		airlineID = newID;
+	}
+	void setAirlineName(string newName)
+	{
+		airlineName = newName;
+	}
+	void setAirlineID(int newID)
+	{
+		airlineID = newID;
+	}
+	string getAirlineName()
+	{
+		return airlineName;
+	}
+	int getAirlineID()
+	{
+		return airlineID;
+	}
+};
+
+class InternationalFlight :virtual public Airline
+{
+private:
+	string country;
+public:
+	InternationalFlight() : Airline()
+	{
+		country = '\0';
+	}
+	InternationalFlight(string newName, int newID, string newCountry = "Nothing set") : Airline(newName, newID)
+	{
+		country = newCountry;
+	}
+	void setCountry(string newCountry)
+	{
+		country = newCountry;
+	}
+	string getCountry()
+	{
+		return country;
+	}
+};
+
+class LocalFlight : virtual public Airline
+{
+private:
+	string city;
+public:
+	LocalFlight() : Airline()
+	{
+		city = '\0';
+	}
+	LocalFlight(string newName, int newID, string newCity = "Nothing set") : Airline(newName, newID)
+	{
+		city = newCity;
+	}
+	void setCity(string newCity)
+	{
+		city = newCity;
+	}
+	string getCity()
+	{
+		return city;
+	}
+};
+
+class Plane : public InternationalFlight, public LocalFlight
+{
+private:
+	Ticket* ticketArray;
+	char statusOfFlight;	//O for ontime, D for delayed, M for Missed
+	int availableTickets;
+	int flightNumber;
+	Time departureTime;
+	Time arrivalTime;
+	Date flightDate;
+public:
+	Plane()
+	{
+		ticketArray = NULL;
+		statusOfFlight = '\0';
+		availableTickets = 0;
+		flightNumber = 0;
+		flightDate.day = 0;
+		flightDate.month = 0;
+		flightDate.year = 0;
+		departureTime.hour = 0;
+		departureTime.minutes = 0;
+		departureTime.seconds = 0;
+		arrivalTime.hour = 0;
+		arrivalTime.minutes = 0;
+		arrivalTime.seconds = 0;
+	}
+	Plane(Ticket* newArr, char newStatus, int newTickets, int newFlightNumber, Time newDeptTime, Time newArrTime, Date newDate, string newName,
+		int newID) : InternationalFlight(newName, newID), LocalFlight(newName, newID)
+	{
+		ticketArray = newArr;
+		statusOfFlight = newStatus;
+		availableTickets = newTickets;
+		flightNumber = newFlightNumber;
+		flightDate.day = newDate.day;
+		flightDate.month = newDate.month;
+		flightDate.year = newDate.year;
+		departureTime.hour = newDeptTime.hour;
+		departureTime.minutes = newDeptTime.minutes;
+		departureTime.seconds = newDeptTime.seconds;
+		arrivalTime.hour = newArrTime.hour;
+		arrivalTime.minutes = newArrTime.minutes;
+		arrivalTime.seconds = newArrTime.seconds;
+	}
+
+	// Setters
+	void setTicketArr(Ticket* newArr)
+	{
+		ticketArray = newArr;
+	}
+	void setStatusOfFlight(char newStatus)
+	{
+		statusOfFlight = newStatus;
+	}
+	void setAvailableTickets(int newAvailableTickets)
+	{
+		availableTickets = newAvailableTickets;
+	}
+	void setFlightNumber(int newFlight)
+	{
+		flightNumber = newFlight;
+	}
+	void setDepartureTime(Time newDeptTime)
+	{
+		departureTime.hour = newDeptTime.hour;
+		departureTime.minutes = newDeptTime.minutes;
+		departureTime.seconds = newDeptTime.seconds;
+	}
+	void setArrivalTime(Time newArrivalTime)
+	{
+		arrivalTime.hour = newArrivalTime.hour;
+		arrivalTime.minutes = newArrivalTime.minutes;
+		arrivalTime.seconds = newArrivalTime.seconds;
+	}
+	void setDate(Date newDate)
+	{
+		flightDate.day = newDate.day;
+		flightDate.month = newDate.month;
+		flightDate.year = newDate.year;
+	}
+
+	// Getters
+	Ticket* getTickets()
+	{
+		return ticketArray;
+	}
+	char getStatusOfFlight()
+	{
+		return statusOfFlight;
+	}
+	int getAvailableTickets()
+	{
+		return availableTickets;
+	}
+	int getUnbookedTickets()
+	{
+		int unbookedTickets = 0;
+		for (int counter = 0; counter < availableTickets - 1; counter++)
+			if (ticketArray[counter].getID() == 0)
+				unbookedTickets++;
+		return unbookedTickets;
+	}
+	int getFlightNumber()
+	{
+		return flightNumber;
+	}
+	Time getDepartureTime()
+	{
+		return departureTime;
+	}
+	Time getArrivalTime()
+	{
+		return arrivalTime;
+	}
+	Date getDate()
+	{
+		return flightDate;
+	}
+};
+
+void MainMenu()
+{
+	system("CLS");
+	cout << "Welcome to the Flight Management System!!" << endl;
+	cout << endl;
+	cout << "Choose one of the options down below!" << endl;
+	cout << "1. For Admin" << endl;
+	cout << "2. For Passenger" << endl;
+	cout << "0. To exit the program" << endl;
+	cout << "=> ";
+}
+
+void LoginPortal(Admin  admin)
+{
+	User tempUser;
+	string tempUsername;
+	string tempPassword;
+	char choice;
+	
+	while (tempUser != admin)
+	{
+
+		system("CLS");
+		cout << "Welcome to Admin Login Portal \n\n\n";
+		cout << "Username: ";
+		cin >> tempUsername;
+		tempUser.setUsername(tempUsername);
+		cout << "\nPassword: ";
+		cin >> tempPassword;
+		tempUser.setPassword(tempPassword);
+
+		//for (int i = 0; i < size; i++)
+		//{
+		if (tempUser == admin)
+		{
+			cout  <<admin.GetAdminName()<< " Welcome to Flight Management System \n";
+		}
+		else
+		{
+			
+			cout << "\nInvalid Username or Password \n\n";
+			cout << "Press y to retry \t Press any key to exit\n";
+			cin >> choice;
+			if (choice == 'y')
+			{
+				continue;
+			}
+			else 
+			{
+				break;
+			}
+			
+		}
+		//}
+	}
+}
+void LoginPortal(Passenger  passenger)
+{
+	User tempUser;
+	string tempUsername;
+	string tempPassword;
+	char choice;
+
+	while (tempUser != passenger)
+	{
+
+		system("CLS");
+		cout << "Welcome to Passenger Login Portal \n\n\n";
+		cout << "Username: ";
+		cin >> tempUsername;
+		tempUser.setUsername(tempUsername);
+		cout << "\nPassword: ";
+		cin >> tempPassword;
+		tempUser.setPassword(tempPassword);
+
+		//for (int i = 0; i < size; i++)
+		//{
+		if (tempUser == passenger)
+		{
+			cout <<passenger.GetPassengerName() << " Welcome to Flight Management System \n";
+		}
+		else
+		{
+
+			cout << "\nInvalid Username or Password \n\n";
+			cout << "Press y to retry \t Press any key to exit\n";
+			cin >> choice;
+			if (choice == 'y')
+			{
+				continue;
+			}
+			else
+			{
+				break;
+			}
+
+		}
+		//}
+	}
+}
+
+void AdminMenu()
+{
+	system("CLS");
+	cout << "Choose whatever you would like to do:" << endl;
+	cout << "1. Search through completed flights" << endl;
+	cout << "2. Add new flights to the database" << endl;
+	cout << "3. Remove flights from the database" << endl;
+	cout << "4. Edit details of a flight" << endl;
+	cout << "5. Display Passengers (Based on flight number, nationality or passport number)" << endl;
+	cout << "6. Search through flights" << endl;
+	cout << "=> ";
+}
+
+void PassengerMenu()
+{
+	system("CLS");
+	cout << "Choose whatever you would like to do:" << endl;
+	cout << "1. Display available flights" << endl;
+	cout << "2. Book a flight" << endl;
+	cout << "3. Edit your details (Passenger details)" << endl;
+	cout << "4. Search through flights" << endl;
+	cout << "=> ";
+}
 
 
 int main()
 {
+	// Testing everything
+	//MainMenu();
+	//AdminMenu();
+	//PassengerMenu();
+	Admin Admin1("sussybaka", "juba", "Susmad");
+	Passenger P1;
+	P1.setUsername("Attack");
+	P1.setPassword("freedom");
+	P1.SetPassengerName("Eren");
+
+	//LoginPortal(P1);
+	LoginPortal(Admin1);
+
+
+
+
+
+
+
 	return 0;
 }
