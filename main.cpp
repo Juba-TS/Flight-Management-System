a0b2f4de9e87e195395837617b94305c605e9d19
lykosic <105546116+lykosic@users.noreply.github.com>
2022-05-18 03:45:49 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -158,7 +158,19 @@ public:
 	{
 		return ticketId;
 	}
+	
+	//operator overloading
+	/*friend ostream& operator << (ostream& out, const Passenger& newPasenger);*/
+	//friend istream& operator >> (istream& in, const Passenger& newPasenger);
 };
+//ostream& operator << (ostream& out, const Passenger& newPasenger)
+//{
+//
+//}
+//istream& operator >> (istream& in, const Passenger& newPasenger)
+//{
+//
+//}
 
 class Admin : public User
 {
@@ -571,6 +583,88 @@ void LoginPortal(Passenger  passenger)
 		//}
 	}
 }
+void SignUp(Passenger passenger)
+{
+	Passenger newPassenger;
+	string tempUsername;
+	string tempPassword;
+	string tempPassengerName;
+	string tempNationality;
+	long long int tempPassportNo;
+	
+
+	char choice,anykey;
+	
+
+	do
+	{
+
+		system("CLS");
+		cout << "Welcome to Passenger Sign Up Portal \n\n\n";
+		cout << "Username: ";
+		cin >> tempUsername;
+		newPassenger.setUsername(tempUsername);
+
+		if (tempUsername == passenger.getUsername())
+		{
+			cout << "The username is already taken \n";
+
+			cout << "Press y to re enter \t Press any key to exit\n";
+			cin >> choice;
+			if (choice == 'y')
+			{
+				continue;
+			}
+			else
+			{
+				break;
+			}
+
+		}
+
+		else
+		{
+			cout << "\nPassword: ";
+			cin >> tempPassword;
+			newPassenger.setPassword(tempPassword);
+
+			//cout << endl << newPassenger.getUsername() << endl << newPassenger.getPassword();
+
+			cout << "\Passenger Name: ";
+			cin >> tempPassengerName;
+			newPassenger.SetPassengerName(tempPassengerName);
+			cout << "\nNationality: ";
+			cin >> tempNationality;
+			newPassenger.SetNationality(tempNationality);
+
+
+			do
+			{
+				
+				cout << "\nPassport No: ";
+				cin >> tempPassportNo;
+				newPassenger.SetPassportNo(tempPassportNo);
+				if (tempPassportNo == passenger.GetPassportNo())
+				{
+					cout << "\nPassport No already exists \n";
+					//system("pause");
+				}
+			} while (newPassenger.GetPassportNo() == passenger.GetPassportNo());			
+		}
+		
+	} while (newPassenger.getUsername() == passenger.getUsername());
+
+	cout << newPassenger.getUsername() << endl;
+	cout << newPassenger.getPassword() << endl;
+	cout << newPassenger.GetPassengerName() << endl;
+	cout << newPassenger.GetNationality() << endl;
+	cout << newPassenger.GetPassportNo() << endl;
+	cout << newPassenger.GetTicketsBooked() << endl;
+	cout << newPassenger.GetTicketIds() << endl;
+
+
+
+}
 
 void AdminMenu()
 {
@@ -608,13 +702,11 @@ int main()
 	P1.setUsername("Attack");
 	P1.setPassword("freedom");
 	P1.SetPassengerName("Eren");
+	P1.SetPassportNo(90078601);
 
 	//LoginPortal(P1);
-	LoginPortal(Admin1);
-
-
-
-
+	//LoginPortal(Admin1);
+	//SignUp(P1);
 
 
 
